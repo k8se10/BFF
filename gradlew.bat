b08b57addd1bf2866c9f29cdb8731fc9eeb00be3
k8se10 <139054443+k8se10@users.noreply.github.com>
2025-08-08 03:06:39 +0100

    Add files via upload

--- gradlew.bat
+++ gradlew.bat
@@ -1,46 +1,8 @@
-@if "%DEBUG%" == "" @echo off
-@rem ##########################################################################
-@rem Gradle startup script for Windows
-@rem ##########################################################################
-
-setlocal
-
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
-
-if defined JAVA_HOME goto findJavaFromJavaHome
-
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto execute
-
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-goto fail
-
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-
-if exist "%JAVA_EXE%" goto execute
-
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-goto fail
-
-:execute
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%APP_HOME%/gradle/wrapper/gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
-goto end
-
-:fail
-exit /b 1
-
-:end
-:endlocal
-exit /b 0
+@echo off
+where gradle >nul 2>&1
+if %ERRORLEVEL%==0 (
+  gradle %*
+) else (
+  echo Gradle is not installed. Install Gradle 8.7 or run 'gradle wrapper' to generate a wrapper.
+  exit /b 1
+)
